076fd48977b380bc4d20a0056401f4571875aeb5
Frits Duindam <92157304+Flitspaal@users.noreply.github.com>
2023-10-21 17:26:10 +0200

    Update GPTest.py

--- GPTest.py
+++ GPTest.py
@@ -1,16 +1,31 @@
 import openai
 
-openai.api_key = input("give me your key or else > ") # input for your key
+openai.api_key = input("give me your key or else > ") #put in your key
 
 while True:
     print("Enter something (or 'exit' to quit): ")
     userInput = input("> ")
-    if userInput.lower() == "exit":
-        break #exit the loop
-    response = openai.Completion.create(
-      engine="text-davinci-003",
-      prompt= userInput,
-      max_tokens=200
-    )
 
+    # program options
+    if userInput.lower() == "exit": # exit the program
+        break #exit the loop
+    if userInput.lower() == "reply": # uses last response to go deeper in on the awnser
+        userInput = input("> ")
+        userInput = userInput + lastReplie
+        response = openai.Completion.create(
+            engine="text-davinci-003",
+            prompt= userInput,
+            max_tokens=200
+        )
+    else: # standard option
+        response = openai.Completion.create(
+          engine="text-davinci-003",
+          prompt= userInput,
+          max_tokens=200
+        )
+
+    # Print response
     print(response.choices[0].text.strip())
+    lastReplie = response.choices[0].text.strip()
+
+print("thanks for using GPTest")
